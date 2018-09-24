1c8e8cdc3bfe9509ed245f3ea2dd0e9de54b5e84
Jay MOULIN <jaymoulin@gmail.com>
2018-09-24 23:37:16 +0200

    Fixes chromium entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # first arg is `http` or `--some-option`
-if [[ "http*" == "$1" ]] || [[ "-*" == "$1" ]] || [[ "about:blank" == "$1" ]] || [[ "$#" == 0 ]]; then
-    set -- chromium-browser --headless --disable-gpu --remote-debugging-address=0.0.0.0 --remote-debugging-port=$CHROME_DEBUG_PORT "$@"
+if [[ "$1" == http* ]] || [[ "$1" == -* ]] || [[ "about:blank" == "$1" ]] || [[ "$#" == 0 ]]; then
+    set -- chromium --headless --disable-gpu --remote-debugging-address=0.0.0.0 --remote-debugging-port=$CHROME_DEBUG_PORT "$@"
 fi
 exec "$@"
 
